73f3cb4feeb84405e76cd12a624d32f842e95494
Srijan Kumar <140880296+SriiTheGoat@users.noreply.github.com>
2024-09-18 12:41:04 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('menu').addEventListener('click', function () {
+          document.getElementById('sidebar').classList.toggle('active');
+          document.querySelector('.content').classList.toggle('active');
+});
+document.getElementById('menuu').addEventListener('click', function () {
+          document.getElementById('sidebar').classList.toggle('active');
+          document.querySelector('.content').classList.toggle('active');
+});
